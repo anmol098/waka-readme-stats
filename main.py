95e27129fdb8083ceb1c04e1adc17e2534800d4c
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-07-25 17:04:13 +0800

    check forked repo contribution ✨

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ def generate_commit_list():
     print("user {} id {}".format(username, id))
 
     result = run_query(createContributedRepoQuery.substitute(username=username))
-    nodes = result["data"]["user"]["repositoriesContributedTo"]["nodes"]
-    repos = [d for d in nodes if d['isFork'] is False]
+    repos = result["data"]["user"]["repositoriesContributedTo"]["nodes"]
+    # repos = [d for d in nodes if d['isFork'] is False]
 
     morning = 0  # 0 - 6
     daytime = 0  # 6 - 12
