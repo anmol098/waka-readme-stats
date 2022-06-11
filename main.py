c83eeb983ae4844c95e2d2f7d481e33a1c68f632
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-06-11 19:13:15 +0200

    Customizing short profile info (bis)

--- main.py
+++ main.py
@@ -494,7 +494,7 @@ def get_short_info(github):
     #     string += "> 🚫 " + translate["Not Opted to Hire"] + "\n > \n"
 
     string += '📜 '
-    string += translate['public repositories'] % public_repo + " " + '\n > \n' if public_repo != 1 else translate[
+    string += translate['public repositories'] % public_repo + " " + '\n\n' if public_repo != 1 else translate[
         'public repository'] % public_repo + " " + '\n\n'
     string += '🔑 '
     string += translate['private repositories'] % private_repo + " " + ' \n > \n' if private_repo != 1 else translate[
