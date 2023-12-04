437c0f161d887eac8edffc82d3a1ecd805105917
caupolicanre <caupolicanre@gmail.com>
2023-12-03 22:22:36 -0300

    Fix progress bar symbol style bug

--- sources/graphics_list_formatter.py
+++ sources/graphics_list_formatter.py
@@ -42,10 +42,10 @@ def make_graph(percent: float):
     :param percent: Completion percent of the progress bar.
     :return: The string progress bar representation.
     """
-    if len(EM.SYMBOL_STYLE) == "":
+    if len(EM.SYMBOL_STYLE) == 2:
+        done_block, empty_block = EM.SYMBOL_STYLE[0], EM.SYMBOL_STYLE[1]   
+    else:
         done_block, empty_block = Symbol.get_symbols(EM.SYMBOL_VERSION)
-    elif len(EM.SYMBOL_STYLE) == 2:
-        done_block, empty_block = EM.SYMBOL_STYLE[0], EM.SYMBOL_STYLE[1]
     percent_quart = round(percent / 4)
     return f"{done_block * percent_quart}{empty_block * (25 - percent_quart)}"
 
