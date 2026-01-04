bdeb9138b6bfb812335c85692da8a58981ec111c
Anshuman Singh <148977651+DataBoySu@users.noreply.github.com>
2026-01-04 17:48:08 +0530

    fixing empty array being sent to numpy

--- sources/graphics_chart_drawer.py
+++ sources/graphics_chart_drawer.py
@@ -1,7 +1,7 @@
 from typing import Dict
 #fixing empty array being sent to numpy
 import numpy as np
-from numpy import arange
+from numpy
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
