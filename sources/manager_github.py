69fda215be4f78fa3c3dd9fef96059deed3a123d
Void_The_Null <anshumanr22@outlook.com>
2026-01-12 10:49:21 +0530

    Update prepare_github_env method documentation
    Updated the prepare_github_env method to reflect changes in authentication and clarify usage.

--- sources/manager_github.py
+++ sources/manager_github.py
@@ -39,8 +39,8 @@ class GitHubManager:
     @staticmethod
     def prepare_github_env():
         """
-        Download and store for future use:
-        - Current GitHub user.
+        - Updated for PyGitHub's v2.x use of Auth
+        - Authenticate with GitHub using token
         - Named repo of the user [username]/[username].
         - Clone of the named repo.
         """
