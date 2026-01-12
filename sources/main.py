cc342efbac3fd28e10b33d850cf6bcf52b92ca61
Void_The_Null <anshumanr22@outlook.com>
2026-01-12 17:36:14 +0530

    Update main.py

--- sources/main.py
+++ sources/main.py
@@ -177,17 +177,13 @@ async def get_stats() -> str:
     if EM.SHOW_PROFILE_VIEWS:
         DBM.i("Adding profile views info...")
         data = GHM.REMOTE.get_views_traffic(per="week")
-        stats += f"![Profile Views](http://img.shields.io/badge/{quote(FM.t('Profile Views'))}-{data.count}-blue?style={quote(EM.BADGE_STYLE)})\n\n"
+        stats += f"![Profile Views](http://img.shields.io/badge/{quote(FM.t('Profile Views'))}-{data['count']}-blue?style={quote(EM.BADGE_STYLE)})\n\n"
 
     if EM.SHOW_LINES_OF_CODE:
         DBM.i("Adding lines of code info...")
         total_loc = sum([yearly_data[y][q][d]["add"] for y in yearly_data.keys() for q in yearly_data[y].keys() for d in yearly_data[y][q].keys()])
         data = f"{intword(total_loc, format='%.2f')} {FM.t('Lines of code')}"
-        stats += (
-            f"![Lines of code](https://img.shields.io/badge/"
-            f"{quote(FM. t('From Hello World I have written'))}-{quote(data)}-blue?"
-            f"style={quote(EM.BADGE_STYLE)})\n\n"
-        )
+        stats += f"![Lines of code](https://img.shields.io/badge/{quote(FM.t('From Hello World I have written'))}-{quote(data)}-blue?style={quote(EM.BADGE_STYLE)})\n\n"
 
     if EM.SHOW_SHORT_INFO:
         stats += await get_short_github_info()
